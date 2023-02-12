2f71c4ccb1f41cb9e887f7cb8d3a5a7c2d8b984f
OpereiraG <npereira1633@gmail.com>
2023-02-12 13:46:43 -0500

    first commit

---
+++ src/File.java
@@ -0,0 +1,13 @@
+public abstract class File {
+
+
+        abstract void open();
+
+        void edit(){
+            System.out.println("Edit the files");
+        }
+        void close(){
+            System.out.println("Close the program");
+        }
+    }
+
